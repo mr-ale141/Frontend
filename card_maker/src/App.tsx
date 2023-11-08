e5dd0a403c83569c3cd1339291e0738a053f83ca
mr-ale141 <ale141@rambler.ru>
2023-11-08 16:45:27 +0300

    Added onKeyUp undo redo

--- card_maker/src/App.tsx
+++ card_maker/src/App.tsx
@@ -2,7 +2,15 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import ToolsPanel from "./components/ToolsPanel/ToolsPanel";
 import WorkSpace from "./components/WorkSpace/WorkSpace";
+import React from "react";
+import { ActionCreators } from "redux-undo";
+import { useDispatch } from "react-redux";
 function App() {
+    const dispatch = useDispatch();
+    document.body.onkeyup = (e) => {
+        if (e.ctrlKey && e.key === "z") dispatch(ActionCreators.undo());
+        if (e.ctrlKey && e.key === "y") dispatch(ActionCreators.redo());
+    };
     return (
         <div className="app">
             <Header />
