fe4fa7127ae15c556457f72aa58538da22c5978c
mr-ale141 <ale141@rambler.ru>
2023-12-12 15:52:29 +0300

    Added save card without image-link

--- card_maker/src/components/Header/Header.tsx
+++ card_maker/src/components/Header/Header.tsx
@@ -1,17 +1,49 @@
 import css from "./Header.module.css";
 import { ActionCreators } from "redux-undo";
-import { useDispatch } from "react-redux";
 import HeadButton from "./HeadButton/HeadButton";
 import undoIcon from "./icons/undo.png";
 import redoIcon from "./icons/redo.png";
+import saveIcon from "./icons/save.png";
+import html2canvas from "html2canvas";
+import { useAppDispatch, useAppSelector } from "../../data/hooks";
+import { sessionState, setSelectedBlock } from "../../data/sessionReducer";
+import React from "react";
 
 function Header() {
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
+    const state = useAppSelector(sessionState);
+    const canvasId = state.session.template.canvas.id;
+    function onMouseDownHandler(event: React.MouseEvent) {
+        const inputNewText = document.getElementById("new-text");
+        if (!inputNewText) {
+            if (!event.isDefaultPrevented()) {
+                dispatch(setSelectedBlock({ id: "", withCtrl: event.ctrlKey }));
+                event.preventDefault();
+            }
+        }
+    }
+    function saveHandler() {
+        dispatch(setSelectedBlock({ id: "", withCtrl: false }));
+        const canvasDiv = document.getElementById(canvasId);
+        html2canvas(canvasDiv as HTMLElement, {
+            allowTaint: true,
+            useCORS: true,
+        }).then((canvas) => {
+            const dataURL = canvas.toDataURL("image/jpeg");
+            const link = document.createElement("a");
+            link.href = dataURL;
+            link.download = "card_maker_image.jpg";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        });
+    }
     return (
-        <div className={css.header}>
+        <div className={css.header} onMouseDown={onMouseDownHandler}>
             <div className={css.logo}>
                 <span>Card Maker</span>
             </div>
+            <HeadButton handler={saveHandler} icon={saveIcon} alt="save" />
             <HeadButton
                 handler={() => dispatch(ActionCreators.undo())}
                 icon={undoIcon}
--- card_maker/src/components/ImageBlock/ImageBlock.tsx
+++ card_maker/src/components/ImageBlock/ImageBlock.tsx
@@ -1,14 +1,37 @@
 import css from "./ImageBlock.module.css";
 import { ImageBlockType } from "../../type/type";
-import React from "react";
 
 type imageBlockProps = {
     block: ImageBlockType;
 };
+
+// async function imageUrlToBase64(url: string) {
+//     try {
+//         const response = await fetch(url);
+//         const blob = await response.arrayBuffer();
+//         const contentType = response.headers.get("content-type");
+//         const base64String = `data:${contentType};base64,${Buffer.from(
+//             blob,
+//         ).toString("base64")}`;
+//         return base64String;
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
 function ImageBlock({ block }: imageBlockProps) {
+    let data = "";
+    // if (block.bgImage.type === "link") {
+    //     imageUrlToBase64(block.bgImage.data).then((base64String) => {
+    //         data = base64String!;
+    //     });
+    // } else {
+    //     data = block.bgImage.data;
+    // }
+    data = block.bgImage.data;
     return (
         <div className={css.image} id={block.id}>
-            <img src={block.bgImage.data} alt="img" />
+            <img src={data} alt="img" />
         </div>
     );
 }
