fb2ff1428de1e7a8c383b3c9dbdd02a6354c771e
mr-ale141 <ale141@rambler.ru>
2024-01-05 22:44:19 +0300

    fixed scale pdfSave

--- card_maker/src/components/Header/handlers/SavePDFHandler.ts
+++ card_maker/src/components/Header/handlers/SavePDFHandler.ts
@@ -1,6 +1,8 @@
 import html2canvas from "html2canvas";
 import { jsPDF } from "jspdf";
 
+const pxInMm = 3.8;
+
 export default function SavePDFHandler(canvasId: string) {
     const canvasDiv = document.getElementById(canvasId);
     html2canvas(canvasDiv as HTMLElement, {
@@ -11,12 +13,13 @@ export default function SavePDFHandler(canvasId: string) {
         let width = canvas.width;
         let height = canvas.height;
         let pdf;
+        console.log(width, ":", height);
         if (width > height) {
             // eslint-disable-next-line new-cap
-            pdf = new jsPDF("l", "px", [width, height]);
+            pdf = new jsPDF("l", "px", [width / pxInMm, height / pxInMm]);
         } else {
             // eslint-disable-next-line new-cap
-            pdf = new jsPDF("p", "px", [height, width]);
+            pdf = new jsPDF("p", "px", [height / pxInMm, width / pxInMm]);
         }
         width = pdf.internal.pageSize.getWidth();
         height = pdf.internal.pageSize.getHeight();
